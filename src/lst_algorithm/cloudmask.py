7bd77ab0e525e0c58cf63d5d37e4d7fed955f234
Channing Tong <117896810+HereIsZephyrus@users.noreply.github.com>
2025-10-15 12:50:09 +0800

    Enrich landsat RS image bands (#5)
    * add new band compute method
    
    * fix import module faule

--- src/lst_algorithm/cloudmask.py
+++ src/lst_algorithm/cloudmask.py
@@ -1,5 +1,29 @@
-# import ee
+import ee
+import logging
 
+logger = logging.getLogger(__name__)
+
+def calc_cloud_cover(image, whole_geometry, mask_method):
+    counting_image = image.clip(whole_geometry)
+    first_band_name = counting_image.bandNames().get(0)
+    total_counting_pixel = counting_image.reduceRegion(
+        reducer = ee.Reducer.count(),
+        geometry = whole_geometry,
+        scale = 30,
+        maxPixels = 1e13
+    ).get(first_band_name).getInfo()
+    if (total_counting_pixel == 0):
+        raise ValueError("the image is not cover the urban area")
+    # the invalid value pixels are cloud coverd pixels
+    cloud_cover_pixel = mask_method(counting_image).reduceRegion(
+        reducer = ee.Reducer.count(),
+        geometry = whole_geometry,
+        scale = 30,
+        maxPixels = 1e13
+    ).get(first_band_name).getInfo()
+    result = (1 - float(cloud_cover_pixel / total_counting_pixel)) * 100
+    logger.debug("cloud cover ratio is 1 - %s/%s = %s%%", cloud_cover_pixel, total_counting_pixel, result)
+    return result
 
 def mask_sr(image):
     """
